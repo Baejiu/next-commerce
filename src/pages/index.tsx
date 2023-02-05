0202a40e7e981e896f15d0f4437197d8fd2e2450
Baejiu <jiumaker@naver.com>
2023-02-05 16:34:44 +0900

    style: add styled-components lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
 import { useEffect, useRef, useState } from 'react'
 
 export default function Home() {
@@ -34,11 +33,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
+      <main>
+        <div>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
+            <code>src/pages/index.tsx</code>
           </p>
           <input ref={inputRef} type="text" placeholder="name" />
           <button onClick={handleClick}>Add Jacket</button>
