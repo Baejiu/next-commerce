747498b30cd79527d8e1f1d15b765dd47e22bf37
Baejiu <jiumaker@naver.com>
2023-02-11 17:33:22 +0900

    feat: add image gallery

---
+++ src/pages/product.tsx
@@ -0,0 +1,39 @@
+// import ImageGallery from 'react-image-gallery';
+
+import Image from "next/image";
+import Carousel from "nuka-carousel";
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1011/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1011/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1012/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1012/250/150/'
+  },
+  {
+    original: 'https://picsum.photos/id/1013/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1014/250/150/',
+  },
+];
+
+export default function Product() {
+  return (
+    <Carousel>
+      {images.map((item) => <Image key={item.original} src={item.original} alt={item.original} width={500} height={300} layout="responsive" />)}
+    </Carousel>
+        )
+    }
